828156b34b9d7d8f2462c96c48e7d8b1ab0c9933
Sébastien JEAN <contact@sebjean.com>
2024-07-16 18:05:22 +0200

    Update Android SDK

--- src/store-ratings.android.ts
+++ src/store-ratings.android.ts
@@ -1,16 +1,16 @@
-import { Application } from '@nativescript/core';
+import { Application, Utils } from '@nativescript/core'
 
 export function initRating() {
 	const android = Application.android
-	const manager = com.google.android.play.core.review.ReviewManagerFactory.create(android.context)
+	const context = Utils.android.getApplicationContext()
+	const manager = com.google.android.play.core.review.ReviewManagerFactory.create(context)
 	const request = manager.requestReviewFlow()
-	request.addOnCompleteListener(new com.google.android.play.core.tasks.OnCompleteListener({
-	  onComplete: task => {
-	    if (task.isSuccessful()) {
-	      const reviewInfo = task.getResult();
-	      manager.launchReviewFlow(android.foregroundActivity, reviewInfo)
-	    }
-	  }
+	request.addOnCompleteListener(new com.google.android.gms.tasks.OnCompleteListener({
+		onComplete: task => {
+			if (task.isSuccessful()) {
+				const reviewInfo = task.getResult()
+				manager.launchReviewFlow(android.foregroundActivity, reviewInfo)
+			}
+		}
 	}))
 }
-
